a82bb0e8c35edec9e29818afb1851b364cb9297d
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2024-11-01 16:22:58 +0200

    Add Maven support
    Add Maven support
    
    Add Maven support

---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,18 @@
+package com.epam;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+
+
+        scanner.close();
+    }
+}
