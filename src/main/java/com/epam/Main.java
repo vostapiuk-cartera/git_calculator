8769d9287c973e5eee65db619fcefe176782d125
RomanNadia <nad.roman14@gmail.com>
2024-11-01 18:29:11 +0200

    multiplication

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -11,8 +11,14 @@ public class Main {
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
 
-
+        System.out.println("The multiplication result of" + num1 + " and " + num2 + " is: "
+                + multiply(num1, num2));
 
         scanner.close();
     }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
 }
