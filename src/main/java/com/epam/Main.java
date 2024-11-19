3678c05476d4cecef1553a0dbfbb2eebf02b5721
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2024-11-19 17:35:18 +0200

    Add divide

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -22,6 +22,10 @@ public class Main {
         return num1 * num2;
     }
 
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
     public static int minus(int num1, int num2) {
         return num1 - num2;
     }
