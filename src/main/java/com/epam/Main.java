5ad2726c0c6f51ac8005d010dc611b80f8eb0586
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2024-11-19 17:37:10 +0200

    Add minus

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -22,6 +22,9 @@ public class Main {
         return num1 * num2;
     }
 
+    public static int minus(int num1, int num2) {
+        return num1 - num2;
+    }
 
     public static int add(int num1, int num2) {
         return num1 + num2;
